b1160024edabfe227ceba343b768fc7091742e1c
jsk18 <67679531+jsk18@users.noreply.github.com>
2020-07-18 15:02:54 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -6,36 +6,13 @@ import json
 import sys
 import os
 from note163 import *
-from wps import *
 
 # 解决https访问警告
 # from requests.packages.urllib3.exceptions import InsecureRequestWarning,InsecurePlatformWarning
 # requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 # requests.packages.urllib3.disable_warnings(InsecurePlatformWarning)
 
-def _wps_checkin(data):
-    sid = data['wps_checkin']
 
-    print("\n            ===模拟[我的wps小程序]签到===")
-    for item in sid:
-        print("\n    为{}签到---↓".format(item['name']))
-        wps_clockin(item['sid'])
-
-
-def _wps_invite(data):
-    wps_inv = data['wps_invite']
-
-    print("\n\n            ===邀请僵尸账号获得额外会员=== ")
-    for item in wps_inv:
-        print("    为{}邀请---↓".format(item['name']))
-        wps_invite(sid=[], invite_userid=item['invite_userid'])
-
-def _docer_checkin(data):
-    sid = data['wps_checkin']
-    print("\n            ===模拟[稻壳会员]签到===")
-    for item in sid:
-        print("    为{}签到---↓".format(item['name']))
-        docer_checkin(item['sid'])
 
 
 def _youdao_checkin(data):
